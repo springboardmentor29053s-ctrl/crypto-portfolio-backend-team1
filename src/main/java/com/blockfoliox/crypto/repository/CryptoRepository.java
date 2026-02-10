869f03ea09a6fedf674dda52f3a50622b6905b89
Aarushi Garg <aarushigarg56@gmail.com>
2026-02-10 18:17:26 +0530

    Delete src directory

--- src/main/java/com/blockfoliox/crypto/repository/CryptoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.blockfoliox.crypto.repository;
-
-import com.blockfoliox.crypto.model.Crypto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CryptoRepository extends JpaRepository<Crypto, Long> {
-}
-
